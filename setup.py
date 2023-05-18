7421434b8d8563284dfc152e6d6892d0b83f1fcb
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2023-05-18 01:56:19 -0400

    Bump version for PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nprr",
-    version="0.1.1",
+    version="0.1.2",
     long_description="file: README.md",
     long_description_content_type="text/markdown",
     description="Nonparametric randomized response and locally private confidence sets",
