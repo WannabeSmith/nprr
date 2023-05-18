ab58aaf39bea3f1a62183994fad3837f8dab01b1
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2023-05-18 02:04:13 -0400

    Update long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="nprr",
-    version="0.1.2",
-    long_description="file: README.md",
+    version="0.1.3",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     description="Nonparametric randomized response and locally private confidence sets",
     url="https://github.com/wannabesmith/nprr",
