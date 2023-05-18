17f14cba55e6928e61b4dab5b530e3d4b591e4eb
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2023-05-18 01:17:22 -0400

    Getting ready for packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="nprr",
-    version="0.1",
+    version="0.1.0",
     description="Nonparametric randomized response and locally private confidence sets",
-    url="github.com/wannabesmith/nprr",
+    url="https://github.com/wannabesmith/nprr",
     author="Ian Waudby-Smith",
     author_email="iwaudbysmith@gmail.com",
     license="BSD 3-Clause",
     packages=["nprr"],
-    install_requires=["confseq", "numpy", "scipy", "matplotlib"],
+    install_requires=["confseq", "numpy", "scipy"],
     zip_safe=False,
 )
